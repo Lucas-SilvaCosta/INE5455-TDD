9746342275ad44da9f405ea5898e016343b3586d
Lucas <sc.lucascosta@gmail.com>
2024-10-23 11:11:26 -0300

    Codigo teste 5 empresa

--- src/empresa.py
+++ src/empresa.py
@@ -1,9 +1,13 @@
-from src.funcionario import Funcionario
+from src.projeto import Projeto
 
 class Empresa:
     def __init__(self, nome):
         self.nome = nome
         self.funcionarios = []
+        self.projetos = []
 
     def adicionaFuncionario(self, funcionario):
         self.funcionarios.append(funcionario)
+    
+    def adicionaProjeto(self, projeto):
+        self.projetos.append(Projeto("Jornada", "Jorge"))
