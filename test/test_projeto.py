315f8c7eafb787bb64d93134ad181a6de980d4cd
Lucas <sc.lucascosta@gmail.com>
2024-10-23 10:30:35 -0300

    Teste 1 projeto

---
+++ test/test_projeto.py
@@ -0,0 +1,17 @@
+import unittest
+
+from src.projeto import Projeto
+from src.funcionario import Funcionario
+
+class TestProjeto(unittest.TestCase):
+    
+    def setUp(self):
+        self.jorge = Funcionario("Jorge", "123")
+
+    def testCriaJornadaJorge(self):
+        jornada = Projeto("Jornada", self.jorge)
+        self.assertEqual("Jornada", jornada.nome)
+        self.assertEqual(self.jorge, jornada.responsavel)
+    
+if __name__ == '__main__':
+    unittest.main()
