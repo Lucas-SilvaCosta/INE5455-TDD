e41c4ca773d92bdb6947b0b237f31cec546755a7
Lucas <sc.lucascosta@gmail.com>
2024-10-23 10:12:47 -0300

    Codigo teste 1 funcionario

---
+++ src/funcionario.py
@@ -0,0 +1,4 @@
+class Funcionario:
+    def __init__(self, nome, cpf):
+        self.nome = "Jorge"
+        self.cpf = "123"
--- test/test_funcionario.py
+++ test/test_funcionario.py
@@ -1,9 +1,15 @@
 import unittest
 
+from src.funcionario import Funcionario
+
 class TestFuncionario(unittest.TestCase):
     
     def testCriaJorge123(self):
         jorge = Funcionario("Jorge", "123")
+        self.assertEquals("Jorge", jorge.name)
+        self.assertEquals("123", jorge.cpf)
+
+    
     
 if __name__ == '__main__':
     unittest.main()
