621d22ad3e68606e9214a09774dfb6c5a17bfbf0
Lucas <sc.lucascosta@gmail.com>
2024-10-23 10:47:30 -0300

    Teste 2 empresa

--- test/test_empresa.py
+++ test/test_empresa.py
@@ -7,7 +7,11 @@ class TestEmpresa(unittest.TestCase):
     
     def testCriaAmbev(self):
         ambev = Empresa("Ambev")
-        self.assertEqual("Ambev", ambev)    
+        self.assertEqual("Ambev", ambev.nome)
+    
+    def testCriaFord(self):
+        ford = Empresa("Ford")
+        self.assertEqual("Ford", ford.nome)    
     
 if __name__ == '__main__':
     unittest.main()
