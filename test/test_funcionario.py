719d73af101d428cc786aa28e7e2a7dfd29eb126
Lucas <sc.lucascosta@gmail.com>
2024-10-23 10:22:05 -0300

    teste 2 funcionario

--- test/test_funcionario.py
+++ test/test_funcionario.py
@@ -6,8 +6,13 @@ class TestFuncionario(unittest.TestCase):
     
     def testCriaJorge123(self):
         jorge = Funcionario("Jorge", "123")
-        self.assertEquals("Jorge", jorge.name)
+        self.assertEquals("Jorge", jorge.nome)
         self.assertEquals("123", jorge.cpf)
+
+    def testCriaMatheus456(self):
+        matheus = Funcionario("Matheus", "456")
+        self.assertEquals("Matheus", matheus.nome)
+        self.assertEquals("456", matheus.cpf)
 
     
     
